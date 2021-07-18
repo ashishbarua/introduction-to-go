ea027104cdc5de2252c86db28788ffeec7b00bb2
ashishbarua <ashishb9000@gmail.com>
2021-07-18 21:36:07 +0700

    Local variables and functions Ended: https://youtu.be/jpKysZwllVw?list=PLHq1uqvAteVufR04uKxK-oDRZqFenJLii&t=1113

--- app/app.go
+++ app/app.go
@@ -3,5 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, world")
+	customer := getData(1)
+	fmt.Println(customer)
+}
+
+func getData(customerId int) (customer string) {
+	firstName := "Ashish"
+	lastName := "Barua"
+	fullName := firstName + " " + lastName
+	return fullName
 }
